17b5e83fac7957dfb222138b31f67467ec23e375
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-11-24 11:58:12 +0600

    feat: Implement AuthProvider and useAuth hook Create AuthProvider component with Firebase authentication methods and user state management. Introduce useAuth hook for easy access to authentication context in components.

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Routes/Routes";
+import AuthProvider from "./provider/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Routes} />
+    <AuthProvider>
+      <RouterProvider router={Routes} />
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,84 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../config/firebase.config";
+
+export const AuthContext = createContext(null);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // login with google
+  const gooleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // creat user
+  const creatUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // singIn user
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // logOut user
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // user update profile
+
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    creatUser,
+    login,
+    logOut,
+    updateUserProfile,
+    gooleSignIn,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.object,
+};
+
+export default AuthProvider;
