6d90d3875bfd79a6e0804e448241dad54914bdae
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-11-24 12:09:12 +0600

    feat: Integrate @tanstack/react-query, react-helmet-async, and AuthProvider Set up @tanstack/react-query for efficient data fetching, integrate react-helmet-async for dynamic head management.

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import Routes from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={Routes} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <RouterProvider router={Routes} />
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title> Home</title>
+      </Helmet>
       <h1>this is home </h1>
     </div>
   );
