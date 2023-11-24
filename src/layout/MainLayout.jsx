e9b857eae06ab9ec06ba1562402baaa323f96281
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-11-24 11:39:11 +0600

    feat: Integrate Firebase for authentication and initialize app Configure Firebase SDK with environment variables and set up authentication. Also, structure the React app with a main layout, home page, and routing using react-router-dom.

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
